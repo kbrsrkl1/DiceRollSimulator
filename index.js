55955085a7509e9308979339a938637c6872b1cb
kbrsrkl1 <kubranur216@gmail.com>
2024-09-29 14:00:15 +0300

    commit

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const buttonEl = document.getElementById("roll-button");
 const diceEl = document.getElementById("dice");
 
 
-
+function rollDice(){
+    const rollResult = Math.floor(Math.random() * 6)+ 1;
+    console.log(rollResult);
+}
 
 buttonEl.addEventListener("click", () => {
     diceEl.classList.add("roll-animation");
-    setTimeout(() => {diceEl.classList.remove("roll-animation");}, 1000);
+    setTimeout(() => {diceEl.classList.remove("roll-animation");
+        rollDice()
+    }, 1000);
 })
 
